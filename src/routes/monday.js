fcdeadf3ff1ddaeeea1e974a6da5f66f3624ae2c
Andrew Lays <andrew@mediawayss.com>
2021-03-18 17:20:43 +0200

    Added routes to clone/sync items by assignee

--- src/routes/monday.js
+++ src/routes/monday.js
@@ -3,7 +3,9 @@ const {authenticationMiddleware} = require('../middlewares/authentication');
 const mondayController = require('../controllers/monday-controller');
 
 router.post('/monday/clone_item', authenticationMiddleware, mondayController.cloneItem);
+router.post('/monday/clone_item_by_person', authenticationMiddleware, mondayController.cloneItemByPerson);
 router.post('/monday/sync_item', authenticationMiddleware, mondayController.syncItem);
+router.post('/monday/sync_item_by_person', authenticationMiddleware, mondayController.syncItemByPerson);
 router.post('/monday/subscribe_team', authenticationMiddleware, mondayController.subscribeTeam);
 router.post('/monday/map_existing_items', authenticationMiddleware, mondayController.mapExistingItems);
 router.post('/monday/recipe_subscribed', authenticationMiddleware, mondayController.recipeSubscribed);
